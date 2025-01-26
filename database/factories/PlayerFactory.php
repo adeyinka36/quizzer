f634df3a2cf244122e77cfd319d1ba976bfc0f37
Adeyinka Giwa <adeyinkagiwa@Adeyinkas-MacBook-Pro.local>
2025-01-26 12:39:11 +0000

    added player seeder

--- database/factories/PlayerFactory.php
+++ database/factories/PlayerFactory.php
@@ -24,11 +24,11 @@ class PlayerFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'username' => fake()->userName(),
+            'password' => Hash::make('Secret2000###'),
         ];
     }
 
